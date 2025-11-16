dbf760afac5a3db63d0e2bbc15a3f074ec9b3f82
Andres Porras <ajporras0412gmail.com>
2025-11-15 20:04:05 -0500

    udpate

--- FRONT_LACARDIO/src/app/app.routes.ts
+++ FRONT_LACARDIO/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirección raíz
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // Vistas principales
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+
+  // Ruta por defecto si no existe (404)
+  { path: '**', redirectTo: 'home' },
+];
